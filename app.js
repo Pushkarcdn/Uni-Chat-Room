1f53959e998b116b75d230a4e6978931b2ada7ce
Pushkar Kumar Sah <pushkarcdn@gmail.com>
2025-06-25 15:33:39 +0545

    fixed bugs

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import express from "express";
 import http from "http";
-import { Server } from 'socket.io';
-import { dirname, join } from 'path';
-import { fileURLToPath } from 'url';
+import { Server } from "socket.io";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 import Messages from "./models/messages.js";
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -13,10 +13,10 @@ const mongoURI = process.env.MONGO_URI;
 const port = process.env.PORT;
 
 try {
-    await mongoose.connect(mongoURI);
-    // console.log("Connected to MongoDB");
+  await mongoose.connect(mongoURI);
+  console.log("Connected to MongoDB");
 } catch (error) {
-    console.log(error);
+  console.log(error);
 }
 
 const app = express();
@@ -30,64 +30,54 @@ app.set("view engine", "ejs");
 app.use(express.static(join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    res.render("index");
+  res.render("index");
 });
 
 app.get("/messages", async (req, res) => {
+  const messages = await Messages.find({});
 
-    const messages = await Messages.find({});
-
-    res.status(200).send(messages);
-
+  res.status(200).send(messages);
 });
 
-
 io.on("connection", (socket) => {
+  const userName = socket.handshake.query.userName;
 
-    const userName = socket.handshake.query.userName;
+  // console.log(userName, " connected");
 
-    // console.log(userName, " connected");
+  // socket.on("disconnect", () => {
+  //     console.log("A user disconnected");
+  // });
 
-    // socket.on("disconnect", () => {
-    //     console.log("A user disconnected");
-    // });
+  socket.on("message", async (userID, userName, message) => {
+    const currentTime = new Date();
+    let hours = currentTime.getHours();
+    let duration = "AM";
+    if (hours > 12) {
+      hours -= 12;
+      duration = "PM";
+    }
+    let minutes = currentTime.getMinutes().toString().padStart(2, "0");
+    let time = `${hours}:${minutes} ${duration}`;
 
-    socket.on("message", async (userID, userName, message) => {
-
-        const currentTime = new Date();
-        let hours = currentTime.getHours();
-        let duration = "AM";
-        if (hours > 12) {
-            hours -= 12;
-            duration = "PM";
-        }
-        let minutes = currentTime.getMinutes().toString().padStart(2, '0');
-        let time = `${hours}:${minutes} ${duration}`;
-
-
-        const newMessage = await Messages.create({
-            userID: userID,
-            userName: userName,
-            message: message,
-            createdAt: currentTime
-        });
-
-        // console.log(userID, userName, message);
-
-        io.emit('message', userID, newMessage._id, userName, message, time);
-
+    const newMessage = await Messages.create({
+      userID: userID,
+      userName: userName,
+      message: message,
+      createdAt: currentTime,
     });
 
-    socket.on("delete", async (messageID) => {
+    // console.log(userID, userName, message);
 
-        await Messages.findByIdAndDelete(messageID);
+    io.emit("message", userID, newMessage._id, userName, message, time);
+  });
 
-        io.emit('updated', "Message Deleted");
+  socket.on("delete", async (messageID) => {
+    await Messages.findByIdAndDelete(messageID);
 
-    });
-
-})
+    io.emit("updated", "Message Deleted");
+  });
+});
 
 server.listen(port, () => {
-    console.log('server running at ' + port);
+  console.log("server running at " + port);
 });
